8cfd708831aecee202db92528f633bb73e7a9f84
Zaregotto <Zaregotto307@gmail.com>
2022-05-20 16:40:26 +0300

    Angular-3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HttpClientModule } from "@angular/common/http";
 
+import { AppRoutingModule } from "./app-routing.module";
+import { HeaderComponent } from './components/header/header.component';
 import { AppComponent } from './app.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainLayoutComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
